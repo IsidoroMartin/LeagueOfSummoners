e44c2f1e7c59d29137502eee9c6b4811767e278b
Juanjo Ramírez Sánchez <juanjoramirezsanchez@gmail.com>
2016-04-18 00:23:42 +0200

    Cambios mil cosasa gain :·(

--- src/main/java/com/leagueofsummoners/model/services/LocaleService.java
+++ src/main/java/com/leagueofsummoners/model/services/LocaleService.java
@@ -21,10 +21,10 @@ public class LocaleService extends LocaleChangeInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws ServletException {
-        boolean result = super.preHandle(request, response, handler);
-        Cookie cookieLanguage = new Cookie("locale", LocaleContextHolder.getLocale().getLanguage());
-        cookieLanguage.setPath("/");
-        response.addCookie(cookieLanguage);
+        boolean result = super.preHandle(request, response, handler); //Invoco al método encargado de settear el locale al pedido por el navegador
+        Cookie cookieLanguage = new Cookie("locale", LocaleContextHolder.getLocale().getLanguage());  //setteo cookie para que desde el cliente podamos ser conscientes del idioma del server
+        cookieLanguage.setPath("/");//La ruta raíz
+        response.addCookie(cookieLanguage); //lo meto en la respuesta
         return result;
     }
 }
