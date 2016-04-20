293dfbf2e3cc747bd17121fabc50210a36274b7e
Juanjo Ramírez Sánchez <juanjoramirezsanchez@gmail.com>
2016-04-20 22:23:17 +0200

    Almost full support to register form

--- src/main/java/com/leagueofsummoners/model/utils/ValidationRegEXP.java
+++ src/main/java/com/leagueofsummoners/model/utils/ValidationRegEXP.java
@@ -5,5 +5,6 @@ package com.leagueofsummoners.model.utils;
  */
 public class ValidationRegEXP {
     public static final String VALIDATE_NAME = "^[a-zA-ZñáéíóúÑÁÉÍÓÚ][a-zA-Z ñáéíóúÑÁÉÍÓÚ]{0,30}$";
+
     public static final String COMPLEX_PASSWORD = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{5,10}";
 }
