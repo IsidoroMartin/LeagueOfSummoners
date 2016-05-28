fd15a2543b4dae693df0a657e0fff72f8144f5d0
Isidoro Martin Fontenla <isi.martinfontenla@gmail.com>
2016-05-28 13:04:33 +0200

    Añadido política de cookies t de privacidad

--- src/main/java/com/leagueofsummoners/ApplicationPaths.java
+++ src/main/java/com/leagueofsummoners/ApplicationPaths.java
@@ -11,51 +11,55 @@ import java.io.IOException;
  */
 public class ApplicationPaths {
 
-	/**
-	 * Generación de rutas dependiendo de donde se este ejecutando la app.
-	 */
-	static {
-		try {
-			// GALLERY_DEVELOP_SAVE_PATH = new File(".").getCanonicalPath() +
-			// File.separator + "src" + File.separator + "main"
-			// + File.separator + "resources" + File.separator + "static" +
-			// File.separator + "img" + File.separator + "avatars" +
-			// File.separator;
+    /**
+     * Generación de rutas dependiendo de donde se este ejecutando la app.
+     */
+    static {
+        try {
+            // GALLERY_DEVELOP_SAVE_PATH = new File(".").getCanonicalPath() +
+            // File.separator + "src" + File.separator + "main"
+            // + File.separator + "resources" + File.separator + "static" +
+            // File.separator + "img" + File.separator + "avatars" +
+            // File.separator;
 
-			GALLERY_DEVELOP_SAVE_PATH = new File(".").getCanonicalPath() + File.separator + "webapps" + File.separator
-					+ "ROOT" + File.separator + "WEB-INF" + File.separator + "classes" + File.separator
-					+ "static" + File.separator + "img" + File.separator + "avatars";
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            GALLERY_DEVELOP_SAVE_PATH = new File(".").getCanonicalPath() + File.separator + "webapps" + File.separator
+                    + "ROOT" + File.separator + "WEB-INF" + File.separator + "classes" + File.separator
+                    + "static" + File.separator + "img" + File.separator + "avatars";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static String GALLERY_DEVELOP_SAVE_PATH;
-	public static final String ROOT_PATH = "/";
-	public static final String INDEX_HTML_PATH = ROOT_PATH + "index.html";
-	public static final String INDEX_PATH = "/index";
-	public static final String HOME_PATH = "/home";
+    public static String GALLERY_DEVELOP_SAVE_PATH;
+    public static final String ROOT_PATH = "/";
+    public static final String INDEX_HTML_PATH = ROOT_PATH + "index.html";
+    public static final String INDEX_PATH = "/index";
+    public static final String HOME_PATH = "/home";
 
-	public static final String CHAMPIONS_PATH = "/champions";
-	public static final String CHAMPIONS_HTML_PATH = "/champions.html";
+    public static final String CHAMPIONS_PATH = "/champions";
+    public static final String CHAMPIONS_HTML_PATH = "/champions.html";
 
-	public static final String SUMMONER_MATCH_STADISTICS_HTML_PATH = "/summonermatchstadistics.html";
-	public static final String SUMMONER_MATCH_STADISTICS_PATH = "/summonermatchstadistics";
+    public static final String SUMMONER_MATCH_STADISTICS_HTML_PATH = "/summonermatchstadistics.html";
+    public static final String SUMMONER_MATCH_STADISTICS_PATH = "/summonermatchstadistics";
 
-	public static final String GUIDES_HTML_PATH = "/guides.html";
-	public static final String GUIDES_BORRAR_PATH = "/borrar/{id}";
-	public static final String GUIDES_PATH = "/guides";
+    public static final String GUIDES_HTML_PATH = "/guides.html";
+    public static final String GUIDES_BORRAR_PATH = "/borrar/{id}";
+    public static final String GUIDES_PATH = "/guides";
 
-	public static final String SEARCH_PATH = "/search";
+    public static final String SEARCH_PATH = "/search";
 
-	// Rest Services Path
+    public static final String COOKIES_PATH = "/cookies";
 
-	public static final String REST_API_USER = "/api/user";
-	public static final String REST_API_USER_USERNAME = "/username";
-	public static final String REST_API_USER_EMAIL = "/email";
-	public static final String REST_API_USER_SUMMONERNAME = "/summonername";
+    public static final String P_PRIVACIDAD_PATH = "/p_privacidad";
 
-	public static final String REST_API_SUMMONER = "/api/summoner";
-	public static final String REST_API_SUMMONER_MATCHLIST = "/matchlist";
+    // Rest Services Path
+
+    public static final String REST_API_USER = "/api/user";
+    public static final String REST_API_USER_USERNAME = "/username";
+    public static final String REST_API_USER_EMAIL = "/email";
+    public static final String REST_API_USER_SUMMONERNAME = "/summonername";
+
+    public static final String REST_API_SUMMONER = "/api/summoner";
+    public static final String REST_API_SUMMONER_MATCHLIST = "/matchlist";
 
 }
