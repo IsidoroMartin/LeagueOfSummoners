fd74e8b1b4c705b14ba0fe828f6908e516481f1d
Juan José Ramírez Sánchez <Juan Jos Ramrez Snchez>
2016-06-09 23:18:56 +0200

    last commit i hope.

--- src/main/java/com/leagueofsummoners/model/utils/LeagueAccessAPI.java
+++ src/main/java/com/leagueofsummoners/model/utils/LeagueAccessAPI.java
@@ -52,10 +52,10 @@ public class LeagueAccessAPI {
 	public static final String RIOT_API_OBTAIN_INFO_MATCHES = "https://euw.api.pvp.net/api/lol/euw/v2.2/match/{matchId}?rankedQueues=RANKED_SOLO_5x5&seasons=SEASON2016&beginTime=1451602800000&api_key="
 			+ LeagueAccessAPI.LEAGUE_OF_LEGENDS_KEY;
 	public static final String RIOT_API_SUMMONER_PROFILE_ICON_PATH = "http://ddragon.leagueoflegends.com/cdn/6.6.1/img/profileicon/";
-	public static final String RIOT_API_TEEMO_ICON = "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/Teemo.png";
-	public static final String RIOT_API_ITEMS_PATH = "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/item/";
+	public static final String RIOT_API_TEEMO_ICON = "http://ddragon.leagueoflegends.com/cdn/6.11.1/img/champion/Teemo.png";
+	public static final String RIOT_API_ITEMS_PATH = "http://ddragon.leagueoflegends.com/cdn/6.11.1/img/item/";
 	public static final String RIOT_API_SPLASH_ART = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
-	public static final String RIOT_API_CHAMPIONS = "http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/";
+	public static final String RIOT_API_CHAMPIONS = "http://ddragon.leagueoflegends.com/cdn/6.11.1/img/champion/";
 	public static final String RIOT_API_NOT_FOUND = "img/notfoundimg.png";
 
 	public static final String RIOT_API_DEFAULT_ROTATION = "[{\"idChampion\":35,\"championName\":\"Shaco\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Shaco.png\"},{\"idChampion\":103,\"championName\":\"Ahri\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Ahri.png\"},{\"idChampion\":23,\"championName\":\"Tryndamere\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Tryndamere.png\"},{\"idChampion\":104,\"championName\":\"Graves\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Graves.png\"},{\"idChampion\":120,\"championName\":\"Hecarim\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Hecarim.png\"},{\"idChampion\":89,\"championName\":\"Leona\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Leona.png\"},{\"idChampion\":44,\"championName\":\"Taric\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Taric.png\"},{\"idChampion\":110,\"championName\":\"Varus\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Varus.png\"},{\"idChampion\":63,\"championName\":\"Brand\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Brand.png\"},{\"idChampion\":32,\"championName\":\"Amumu\",\"championIconName\":\"http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/Amumu.png\"}]";
